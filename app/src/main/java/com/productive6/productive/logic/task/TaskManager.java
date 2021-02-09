30fb9f38dff4ceaeb8369bd8c26947a806bbde01
Speuce <mrk5199@gmail.com>
2021-02-09 15:18:48 -0600

    Added TaskManager interface

---
+++ app/src/main/java/com/productive6/productive/logic/task/TaskManager.java
@@ -0,0 +1,46 @@
+package com.productive6.productive.logic.task;
+
+import com.productive6.productive.objects.Task;
+
+import java.util.List;
+
+/**
+ * Interface for interacting with the logic layer with regards to tasks
+ *
+ * Implementing classes will handle verification and validation of data input
+ *
+ * @author Matt Kwiatkowski
+ */
+public interface TaskManager {
+
+    /**
+     * @return a list of the users current incomplete tasks,
+     * sorted by highest priority first.
+     */
+    List<Task> getTasksByPriority();
+
+    /**
+     * @return a list of the users current incomplete tasks,
+     * sorted by oldest creation first.
+     */
+    List<Task> getTasksByCreation();
+
+    /**
+     * Adds a new task to the users list of tasks
+     * @param t the {@link Task} to add
+     */
+    void addTask(Task t);
+
+    /**
+     * @return a list of {@link Task} objects that have been marked as
+     * completed
+     */
+    List<Task> getCompletedTasks();
+
+    /**
+     * Updates an task that is already in the system,
+     * i.e changing priority or info
+     * @param t the {@link Task} to change.
+     */
+    void updateTask(Task t);
+}
---
+++ app/src/main/java/com/productive6/productive/logic/task/impl/DummyTaskManager.java
@@ -0,0 +1,9 @@
+package com.productive6.productive.logic.task.impl;
+
+/**
+ * A dummy/test implementation of the {@link com.productive6.productive.logic.task.TaskManager}
+ * interface for unit testing purposes
+ * @author Matt Kwiatkowski
+ */
+public class DummyTaskManager {
+}
