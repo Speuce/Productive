5dfc32564f45252eda3f65748700d63fc17f836f
Speuce <mrk5199@gmail.com>
2021-02-16 09:35:15 -0600

    Update PersistentIDAssignmentException.java

--- app/src/main/java/com/productive6/productive/logic/exceptions/PersistentIDAssignmentException.java
+++ app/src/main/java/com/productive6/productive/logic/exceptions/PersistentIDAssignmentException.java
@@ -2,7 +2,8 @@ package com.productive6.productive.logic.exceptions;
 
 /**
  * This exception should be thrown when persistent entities (annotated with the {@link androidx.room.Entity} interface)
- * are pre-maturely assigned an id, when one shouldn't have been assigned
+ * are pre-maturely assigned an id, when one shouldn't have been assigned,
+ * OR when no id was assigned, yet an assigned id is expected.
  */
 public class PersistentIDAssignmentException extends RuntimeException {
 
