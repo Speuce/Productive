79ae36e94f926ff127eb0eba232f5a2d7052f0b9
Speuce <mrk5199@gmail.com>
2021-02-16 09:40:58 -0600

    Add DataManager Implementations
    Both an in-memory and saved implementations are needed. Saved for production and in memory for system/integration tests.

---
+++ app/src/main/java/com/productive6/productive/persistence/InMemoryDataManager.java
@@ -0,0 +1,38 @@
+package com.productive6.productive.persistence;
+
+import android.content.Context;
+
+import androidx.room.Room;
+
+import com.productive6.productive.objects.Task;
+
+/**
+ * A {@link DataManager} implementation for system and integration testing
+ * using an in-memory database that will not be persisted between runs.
+ */
+public class InMemoryDataManager implements DataManager{
+
+    /**
+     * A reference to the actual app object
+     * provided by android
+     */
+    private final Context context;
+
+    private ProductiveDB db;
+
+    public InMemoryDataManager(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void init() {
+        db = Room.inMemoryDatabaseBuilder(context,
+                ProductiveDB.class).allowMainThreadQueries().build();
+    }
+
+    @Override
+    public TaskPersistenceManager task() {
+        return db.getTaskDao();
+    }
+    
+}
