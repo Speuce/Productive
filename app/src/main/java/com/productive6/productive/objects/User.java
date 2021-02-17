e841168ced6ebe5f0794fdb820ba42ccb1562a6f
Speuce <mrk5199@gmail.com>
2021-02-16 21:24:22 -0600

    Create User.java
    Create an internal representation of a user to track exp, coins, level, etc.

---
+++ app/src/main/java/com/productive6/productive/objects/User.java
@@ -0,0 +1,106 @@
+package com.productive6.productive.objects;
+
+/**
+ * Encapsulates information of a user including XP, Coins, and applicable level
+ */
+public class User {
+
+    /**
+     * id -- for persistence purposes.
+     */
+    private long id;
+
+    /**
+     * The amount of experience
+     */
+    private int exp;
+
+    /**
+     * The current level of the user
+     */
+    private int level;
+
+    /**
+     * The amount of coins that the user has
+     */
+    private int coins;
+
+    /**
+     * The currently-selected title for the user
+     */
+    private String selectedTitle;
+
+    /**
+     * The nothing constructor
+     */
+    public User() {
+    }
+
+    /**
+     * Construct a new user object
+     * @param exp The amount of experience
+     * @param level The current level of the user
+     * @param coins The amount of coins that the user has
+     */
+    public User(int exp, int level, int coins) {
+        this.exp = exp;
+        this.level = level;
+        this.coins = coins;
+    }
+
+    /**
+     * @return The amount of experience
+     */
+    public int getExp() {
+        return exp;
+    }
+
+    /**
+     * @param exp The amount of experience
+     */
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    /**
+     * @return The current level of the user
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * @param level The current level of the user
+     */
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    /**
+     * @return The amount of coins that the user has
+     */
+    public int getCoins() {
+        return coins;
+    }
+
+    /**
+     * @param coins The amount of coins that the user has
+     */
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    /**
+     * @return The currently-selected title for the user
+     */
+    public String getSelectedTitle() {
+        return selectedTitle;
+    }
+
+    /**
+     * @param selectedTitle The currently-selected title for the user
+     */
+    public void setSelectedTitle(String selectedTitle) {
+        this.selectedTitle = selectedTitle;
+    }
+}
