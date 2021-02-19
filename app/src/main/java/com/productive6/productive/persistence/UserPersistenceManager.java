4571cc6041cc9a89038dd3915d1e47598d7251a2
Speuce <mrk5199@gmail.com>
2021-02-19 10:48:03 -0600

    Add UserPersistenceManager and Dao
    These interfaces will automagically get implementations for the interfaces defined CRUD operations with users.

---
+++ app/src/main/java/com/productive6/productive/persistence/UserPersistenceManager.java
@@ -0,0 +1,34 @@
+package com.productive6.productive.persistence;
+
+import androidx.room.Insert;
+
+import com.productive6.productive.objects.User;
+
+import java.util.List;
+
+/**
+ * Interface specifically handling the persistence of users
+ */
+public interface UserPersistenceManager {
+
+
+    /**
+     * Inserts a new (no-id)
+     * task into the database.
+     * An id is automatically added to the object.
+     * @param u the task to add.
+     */
+    void insertUser(User u);
+
+    /**
+     * @return a list of all users. Ordering is inspecific.
+     */
+    List<User> getAllUsers();
+
+    /**
+     * Updates the database information of a user
+     * which has already been assigned an id
+     * @param u the task to update
+     */
+    void updateUser(User u);
+}
---
+++ app/src/main/java/com/productive6/productive/persistence/daos/UserDao.java
@@ -0,0 +1,36 @@
+package com.productive6.productive.persistence.daos;
+
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.productive6.productive.objects.User;
+import com.productive6.productive.persistence.UserPersistenceManager;
+
+import java.util.List;
+
+public interface UserDao extends UserPersistenceManager {
+
+    /**
+     * Inserts a new (no-id)
+     * task into the database.
+     * An id is automatically added to the object.
+     * @param u the task to add.
+     */
+    @Insert
+    void insertUser(User u);
+
+    /**
+     * @return a list of all users. Ordering is inspecific.
+     */
+    @Query("SELECT * FROM users")
+    List<User> getAllUsers();
+
+    /**
+     * Updates the database information of a user
+     * which has already been assigned an id
+     * @param u the task to update
+     */
+    @Update
+    void updateUser(User u);
+}
