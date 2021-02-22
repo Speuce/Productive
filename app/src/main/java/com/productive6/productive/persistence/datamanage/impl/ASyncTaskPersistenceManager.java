965d111da965b04d4c4491a4e57611610a3b7204
Speuce <mrk5199@gmail.com>
2021-02-22 14:06:47 -0600

    Add PersistenceManager implementations

---
+++ app/src/main/java/com/productive6/productive/persistence/datamanage/impl/ASyncTaskPersistenceManager.java
@@ -0,0 +1,58 @@
+package com.productive6.productive.persistence.datamanage.impl;
+
+import com.productive6.productive.executor.RunnableExecutor;
+import com.productive6.productive.objects.Task;
+import com.productive6.productive.persistence.access.ITaskAccess;
+import com.productive6.productive.persistence.datamanage.ITaskPersistenceManager;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Thread-safe implementation of the {@link com.productive6.productive.persistence.datamanage.ITaskPersistenceManager} interface.
+ * Essentailly a thread safe wrapper for the given {@link ITaskAccess}
+ */
+public class ASyncTaskPersistenceManager implements ITaskPersistenceManager {
+
+    /**
+     * For accessing different threads
+     */
+    private final RunnableExecutor executor;
+
+    /**
+     * For accessing the actual database.
+     */
+    private final ITaskAccess access;
+
+    public ASyncTaskPersistenceManager(RunnableExecutor executor, ITaskAccess access) {
+        this.executor = executor;
+        this.access = access;
+    }
+
+    @Override
+    public void insertTask(final Task t) {
+        executor.runASync(() ->access.insertTask(t));
+    }
+
+    @Override
+    public void getAllTasks(final Consumer<List<Task>> callback) {
+        executor.runASync(() -> {
+            List<Task> tasks = access.getAllTasks();
+            executor.runSync(() -> callback.accept(tasks));
+        });
+    }
+
+    @Override
+    public void getAllTasks(final boolean complete, final Consumer<List<Task>> callback) {
+        executor.runASync(() -> {
+            List<Task> tasks = access.getAllTasks(complete);
+            executor.runSync(() -> callback.accept(tasks));
+        });
+    }
+
+    @Override
+    public void updateTask(final Task t) {
+        executor.runASync(() ->access.updateTask(t));
+    }
+}
---
+++ app/src/main/java/com/productive6/productive/persistence/datamanage/impl/ASyncUserPersistenceManager.java
@@ -0,0 +1,52 @@
+package com.productive6.productive.persistence.datamanage.impl;
+
+import com.productive6.productive.executor.RunnableExecutor;
+import com.productive6.productive.objects.Task;
+import com.productive6.productive.objects.User;
+import com.productive6.productive.persistence.access.ITaskAccess;
+import com.productive6.productive.persistence.access.IUserAccess;
+import com.productive6.productive.persistence.datamanage.ITaskPersistenceManager;
+import com.productive6.productive.persistence.datamanage.IUserPersistenceManager;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Thread-safe implementation of the {@link IUserPersistenceManager} interface.
+ * Essentailly a thread safe wrapper for the given {@link IUserAccess}
+ */
+public class ASyncUserPersistenceManager implements IUserPersistenceManager {
+
+    /**
+     * For accessing different threads
+     */
+    private final RunnableExecutor executor;
+
+    /**
+     * For accessing the actual database.
+     */
+    private final IUserAccess access;
+
+    public ASyncUserPersistenceManager(RunnableExecutor executor, IUserAccess access) {
+        this.executor = executor;
+        this.access = access;
+    }
+
+    @Override
+    public void insertUser(final User u) {
+        executor.runASync(() -> access.insertUser(u));
+    }
+
+    @Override
+    public void getAllUsers(Consumer<List<User>> callback) {
+        executor.runASync(() -> {
+            List<User> users = access.getAllUsers();
+            executor.runSync(() -> callback.accept(users));
+        });
+    }
+
+    @Override
+    public void updateUser(final User u) {
+        executor.runASync(() -> access.updateUser(u));
+    }
+}
