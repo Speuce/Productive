f145eca71e51ab76093eed3ac4ba07543ff52ac2
Luke <lukebmorrow@gmail.com>
2021-02-21 22:34:05 -0600

    Removed unnecessary variable.
    Removed unused variable in taskAdapter

--- app/src/main/java/com/productive6/productive/ui/dashboard/TaskAdapter.java
+++ app/src/main/java/com/productive6/productive/ui/dashboard/TaskAdapter.java
@@ -20,8 +20,7 @@ import java.util.ArrayList;
  * fields inside the task list activity all in one easy-to-access place.
  */
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
-
-    private TextView taskName;
+    
     private ArrayList<Task> tasks = new ArrayList<>();
 
     /**
