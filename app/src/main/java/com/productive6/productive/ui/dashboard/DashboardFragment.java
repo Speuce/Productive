6cde72dcd6c0aff33d67d95b76038e63ee5a56c6
FarabiHussain <hussammf@myumanitoba.ca>
2021-02-25 17:46:48 -0600

    Fixed date-related conflicts, code cleanup

--- app/src/main/java/com/productive6/productive/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/productive6/productive/ui/dashboard/DashboardFragment.java
@@ -32,10 +32,8 @@ public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
         attachTaskView(root);
--- app/src/main/java/com/productive6/productive/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/productive6/productive/ui/notifications/NotificationsFragment.java
@@ -24,6 +24,7 @@ import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.productive6.productive.MainActivity;
 import com.productive6.productive.R;
+import com.productive6.productive.logic.event.EventDispatch;
 import com.productive6.productive.logic.task.ITaskManager;
 import com.productive6.productive.objects.Task;
 import com.productive6.productive.ui.dashboard.TaskAdapter;
@@ -34,44 +35,67 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import javax.inject.Inject;
 
 public class NotificationsFragment extends Fragment {
+    private static final String TAG = "NotificationsFragment";
+
+    // Views
     private TextView showMonthName;
     private TextView clickedDate;
-    private String dateInString;
-    private String month;
     private RecyclerView taskDisplayView;
-    private ArrayList<Task> tasks;
-    private ArrayList<Task> filteredByDateTasks;
     private Calendar calendar;
+    private CompactCalendarView calendarView;
     @Inject
     ITaskManager taskManager;
 
+    // Variables
+    private String dateInString;
+    private String month;
+    private ArrayList<Task> tasks;
+    private ArrayList<Task> filteredByDateTasks;
+    private int yearOffset = 1900; // Not sure why but passing the year as parameter starts from 1900, so 2021 is 121 (2021-1900)
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
 
+        initViews(root);
+
+        return root;
+    }
+
+    private void initViews(View root) {
+        calendar = (Calendar.getInstance());
+        calendar.setTime(new Date());
         month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
         taskDisplayView = root.findViewById(R.id.taskDisplayView); //Grab display
-        CompactCalendarView calendarView = root.findViewById(R.id.calendarView);
         clickedDate = root.findViewById(R.id.selectedDateTextView);
         showMonthName = root.findViewById(R.id.monthNameTextView);
         showMonthName.setText(month + " " + (calendar.get(Calendar.YEAR)));
         clickedDate.setText("Tasks scheduled for " + getDateWithSuffix(calendar.get(Calendar.DATE)) + " " + month);
         filteredByDateTasks = new ArrayList<>();
+        dateInString = calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH) + 1) + "-" + calendar.get(Calendar.DATE);
+
+        initTasksArrayList(); // initializing tasks for the RecyclerView
+        initCompactCalendarView(root); // initializing tasks for the CompactCalendarView
+        initializeTaskList(dateInString, root);
+    }
+
+    private void initTasksArrayList() {
         tasks = new ArrayList<>();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-M-d");
-//        System.out.println(dateFormat.format(new Date()));
+        tasks.add(new Task("Buy groceries", 1, 1, 1, new Date(2021 - yearOffset, 1, 24), false));
+        tasks.add(new Task("Finish assignment", 1, 1, 1, new Date(2021 - yearOffset, 1, 25), false));
+        tasks.add(new Task("Clean the house", 1, 1, 1, new Date(2021 - yearOffset, 1, 25), false));
+        tasks.add(new Task("Submit assignment", 1, 1, 1, new Date(2021 - yearOffset, 1, 25), false));
+    }
 
-        tasks.add(new Task("Buy groceries", 1, 1, 1, new Date("2021-2-24"), false));
-        tasks.add(new Task("Finish assignment", 1, 1, 1, new Date("2021-2-24"), false));
-        tasks.add(new Task("Clean the house", 1, 1, 1, new Date("2021-2-24"), false));
-        tasks.add(new Task("Submit assignment", 1, 1, 1, new Date("2021-2-25"), false));
-
+    private void initCompactCalendarView(View root) {
+        calendarView = root.findViewById(R.id.calendarView);
         calendarView.addEvent(new Event(Color.BLUE, 1614146400000L, null)); //24 Feb 2021
         calendarView.addEvent(new Event(Color.BLUE, 1614232800000L, null)); //25 Feb 2021
 
@@ -91,11 +115,7 @@ public class NotificationsFragment extends Fragment {
                 showMonthName.setText(month + " " + calendar.get(Calendar.YEAR));
             }
         });
-
-        initializeTaskList(dateInString, root);
-        return root;
     }
-
 
     private String getDateWithSuffix(int day) {
         if (day >= 11 && day <= 13) {
@@ -115,10 +135,12 @@ public class NotificationsFragment extends Fragment {
 
     // Modified from Luke's code in the dashboard fragment
     private void initializeTaskList(String selectedDate, View root) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M-d");
         filteredByDateTasks.clear();
 
         for (Task task : tasks) {
-            if (task.getDueDate().equals(selectedDate)) {
+            dateInString = sdf.format(task.getDueDate());
+            if (dateInString.equals(selectedDate)) {
                 filteredByDateTasks.add(task);
             }
         }
