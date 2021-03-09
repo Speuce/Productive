b15515e28456d8a44251bc0971eab97072b152fe
Speuce <mrk5199@gmail.com>
2021-03-09 17:02:31 -0600

    Create TaskPopup, refactor to be DRY
    We originally had TaskPopup created twice, once for edit actions and once for add actions. In reality, much of this code only needed to be completed once. This, this commit solves that problem.

--- app/src/main/java/com/productive6/productive/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/productive6/productive/ui/dashboard/DashboardFragment.java
@@ -31,6 +31,8 @@ import com.productive6.productive.logic.task.ITaskSorter;
 import com.productive6.productive.objects.Task;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -48,20 +50,8 @@ public class DashboardFragment extends Fragment {
     @Inject
     ITaskManager taskManager;
 
-    private View popupView;
-
-    private PopupWindow popupWindow;
-
-    private DatePickerDialog datePickerDialog;
-
-    private Calendar calendar;
-
-    private View root;
-
-    /**
-     * For formatting dates in the view
-     */
-    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.CANADA);
+    @Inject
+    ITaskSorter taskSorter;
 
 
     /**
@@ -72,7 +62,7 @@ public class DashboardFragment extends Fragment {
      * @return
      */
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
         attachTaskView(root);
 
@@ -92,166 +82,16 @@ public class DashboardFragment extends Fragment {
         taskDisplayView.setLayoutManager(new LinearLayoutManager(root.getContext(), LinearLayoutManager.VERTICAL, false));//Describe how the data should be laid out
 
         EventDispatch.registerListener(taskAdapter);
-        taskSorter.getTasksByPriority(new TaskConsumerStartup(taskAdapter));//Logic CALL--Load Tasks
 
-        initPopupWindow();
-
-    }
-
-    /**
-     * Holds a callback for the database request made in attachTaskView.
-     */
-    public static class TaskConsumerStartup implements Consumer<List<Task>> {
-        private final TaskAdapter taskAdapter;
-
-        TaskConsumerStartup(TaskAdapter taskAdapter){this.taskAdapter = taskAdapter;}
-
-        @Override
-        public void accept(List<Task> tasks) {
-            taskAdapter.setTasks(tasks);
-            //Give data to Task list and automatically re-renders the Task list view
-        }
-    }
-
-    /**
-     * Initializes the 'create task' popup window.
-     * code from: https://stackoverflow.com/questions/5944987/how-to-create-a-popup-window-popupwindow-in-android
-     *
-     */
-    protected void initPopupWindow(){
-
-        // inflate the layout of the popup window
-        LayoutInflater inflater = (LayoutInflater)
-                getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        popupView = inflater.inflate(R.layout.new_task_popup, null);
-        // create the popup window
-        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        boolean focusable = true; // lets taps outside the popup also dismiss it
-        popupWindow = new PopupWindow(popupView, width, height, focusable);
-        popupWindow.setFocusable(true);
-
-        popupView.setOnTouchListener((v, event) -> {
-            InputMethodManager imm =  (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-            popupView.requestFocus();
-            v.performClick();
-            return false;
-        });
+        taskSorter.getTasksByPriority((taskAdapter::setTasks));//logic call:: get tasks, provide it to task adapter
 
         Button createButton = root.findViewById(R.id.newTaskButton);
-        //Shows 'add task' popup on 'add' button press.
-        createButton.setOnClickListener(v->{
-            popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
-            dimBehind(popupWindow);
-            initDatePicker();
-            initPriority();
-            initDifficulty();
-
-            //On pressing the 'submit' button on the 'add task' popup, create new task object and submit it to the database.
-            Button submitButton = popupView.findViewById(R.id.submit);
-            submitButton.setOnClickListener(view->{
-                //get priority choice
-                RadioGroup radioGroup = popupView.findViewById(R.id.priorityGroup);
-                RadioButton radioButton = radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
-                int priority = radioGroup.indexOfChild(radioButton);
-
-                //get difficulty choice
-                RadioGroup radioDiffGroup = popupView.findViewById(R.id.difficultyGroup);
-                RadioButton radioDiffButton = radioDiffGroup.findViewById(radioDiffGroup.getCheckedRadioButtonId());
-                int difficulty = radioDiffGroup.indexOfChild(radioDiffButton);
-
-                EditText name = popupView.findViewById(R.id.taskNameForm);
-                SwitchCompat hasDeadline = popupView.findViewById(R.id.switchDeadline);
-                //add task
-                if (hasDeadline.isChecked()) {
-                    taskManager.addTask(new Task(name.getText().toString(),priority+1,difficulty+1,0, calendar.getTime(),false));
-                } else {
-                    taskManager.addTask(new Task(name.getText().toString(),priority+1,difficulty+1,0, null,false));
-                }
-
-                popupWindow.dismiss();
-                name.setText("");
-            });
-        });
-    }
-
-    /**
-     * Dims the background behind a given popup window
-     * @param popupWindow the popupwindow to dim around
-     * Code from: https://stackoverflow.com/a/46711174/6047183
-     */
-    public void dimBehind(PopupWindow popupWindow) {
-        View container = popupWindow.getContentView().getRootView();
-        Context context = popupWindow.getContentView().getContext();
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
-        p.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-        p.dimAmount = 0.5f;
-        wm.updateViewLayout(container, p);
-    }
-
-    /**
-     * Initializes the date picker window
-     */
-    private void initDatePicker() {
-        Button dateButton = popupView.findViewById(R.id.datePickerButton);
-        dateButton.setText(format.format(Calendar.getInstance().getTime()));
-        SwitchCompat hasDeadline = popupView.findViewById(R.id.switchDeadline);
-        hasDeadline.setChecked(false);
-        dateButton.setTextColor(Color.GRAY);
-        dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
-
-        //Toggle the button if this task has due date
-        hasDeadline.setOnClickListener(v -> {
-            if (hasDeadline.isChecked()) {
-                dateButton.setTextColor(Color.BLACK);
-                dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.BLACK));
-            } else {
-                dateButton.setTextColor(Color.GRAY);
-                dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
-            }
+        createButton.setOnClickListener(v -> {
+            new TaskPopup(getContext(), null, (task) ->{
+              taskManager.addTask(task);
+            }).open(v);
         });
 
-        //Set the calendar date to the date user selected
-        DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
-            hasDeadline.setChecked(true);
-            dateButton.setTextColor(Color.BLACK);
-            dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.BLACK));
-            calendar = Calendar.getInstance();
-            calendar.set(year, month, dayOfMonth);
-            Date date = calendar.getTime();
-            dateButton.setText(format.format(date));
-        };
-
-        //Initialize date to today when opening the date picker
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        //Initialize date picker dialog
-        datePickerDialog = new DatePickerDialog(getActivity(), dateSetListener, year, month, day);
-        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-
-        //On clicking on date picker button on popup window, open dialog to choose date.
-        dateButton.setOnClickListener(v->datePickerDialog.show());
     }
 
-    /**
-     * Initializes the priority default choice
-     */
-    private void initPriority() {
-        RadioButton defaultChoice = popupView.findViewById(R.id.lowButton);
-        defaultChoice.setChecked(true);
-
-    }
-
-    /**
-     * Initializes the difficulty default choice
-     */
-    private void initDifficulty() {
-        RadioButton defaultChoice = popupView.findViewById(R.id.easyButton);
-        defaultChoice.setChecked(true);
-    }
 }
--- app/src/main/java/com/productive6/productive/ui/dashboard/TaskAdapter.java
+++ app/src/main/java/com/productive6/productive/ui/dashboard/TaskAdapter.java
@@ -64,14 +64,6 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> im
 
     private final View rootView;
 
-    private View popupView;
-
-    private PopupWindow popupWindow;
-
-    private DatePickerDialog datePickerDialog;
-
-    private Calendar calendar;
-
     /**
      * For formatting dates in the view
      */
@@ -87,8 +79,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> im
         private final TextView taskDifficulty;
         private final TextView taskDueDate;
         private final Button taskComplete;
-        private final Button editTask;
-        Task task;
+
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
@@ -97,9 +88,9 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> im
             taskDifficulty = itemView.findViewById(R.id.taskDifficultyTextView);
             taskDueDate = itemView.findViewById(R.id.taskDueDateTextView);
             taskComplete = itemView.findViewById(R.id.taskCompleteToggleButton);
-            editTask = itemView.findViewById(R.id.editButton);
+            Button editTask = itemView.findViewById(R.id.editButton);
             id = itemView.findViewById(R.id.taskId);
-            initPopupWindow();
+//            initPopupWindow();
 
             //listener on 'complete'
             taskComplete.setOnClickListener(v ->{
@@ -113,173 +104,12 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> im
             });
 
             //show edit popup window by clicking 'edit'
-            editTask.setOnClickListener(this::editPopupWindow);
-        }
-
-        /**
-         * Initializes the 'edit task' popup window.
-         * code from: https://stackoverflow.com/questions/5944987/how-to-create-a-popup-window-popupwindow-in-android
-         *
-         */
-        protected void initPopupWindow(){
-
-            // inflate the layout of the popup window
-            LayoutInflater inflater = (LayoutInflater)
-                    itemView.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            popupView = inflater.inflate(R.layout.new_task_popup, null);
-            // create the popup window
-            int width = LinearLayout.LayoutParams.WRAP_CONTENT;
-            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-            boolean focusable = true; // lets taps outside the popup also dismiss it
-            popupWindow = new PopupWindow(popupView, width, height, focusable);
-            popupWindow.setFocusable(true);
-
-            popupView.setOnTouchListener((v, event) -> {
-                InputMethodManager imm =  (InputMethodManager) itemView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                popupView.requestFocus();
-                v.performClick();
-                return false;
+            editTask.setOnClickListener((v) -> {
+                new TaskPopup(itemView.getContext(), tasks.get(getAdapterPosition()), (task) ->{
+                    taskManager.updateTask(task);
+                    updateData();
+                }).open(v);
             });
-        }
-
-        /**
-         * Open popup window of "edit task"
-         * @param view View of edit popup window
-         */
-        public void editPopupWindow(View view) {
-            task = tasks.get(getAdapterPosition());
-            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-            dimBehind(popupWindow);
-
-            //Display info of selected task on popup window
-            initDatePicker();
-            initPriority();
-            initDifficulty();
-
-            EditText name = popupView.findViewById(R.id.taskNameForm);
-            name.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-
-            //Submit button to update info
-            Button submit = popupView.findViewById(R.id.submit);
-            submit.setOnClickListener(v -> {
-                //update task name
-                task.setTaskName(name.getText().toString());
-
-                //update priority choice
-                RadioGroup radioGroup = popupView.findViewById(R.id.priorityGroup);
-                RadioButton radioButton = radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
-                int priority = radioGroup.indexOfChild(radioButton);
-                task.setPriority(priority+1);
-
-                //update difficulty choice
-                RadioGroup radioDiffGroup = popupView.findViewById(R.id.difficultyGroup);
-                RadioButton radioDiffButton = radioDiffGroup.findViewById(radioDiffGroup.getCheckedRadioButtonId());
-                int difficulty = radioDiffGroup.indexOfChild(radioDiffButton);
-                task.setDifficulty(difficulty+1);
-
-                //update due date choice
-                SwitchCompat hasDeadline = popupView.findViewById(R.id.switchDeadline);
-                if (hasDeadline.isChecked()) {
-                    task.setDueDate(calendar.getTime());
-                } else {
-                    task.setDueDate(null);
-                }
-                taskManager.updateTask(task);
-                popupWindow.dismiss();
-                updateData();
-            });
-        }
-
-        /**
-         * Dims the background behind a given popup window
-         * @param popupWindow the popupwindow to dim around
-         * Code from: https://stackoverflow.com/a/46711174/6047183
-         */
-        public void dimBehind(PopupWindow popupWindow) {
-            View container = popupWindow.getContentView().getRootView();
-            Context context = popupWindow.getContentView().getContext();
-            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-            WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
-            p.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-            p.dimAmount = 0.5f;
-            wm.updateViewLayout(container, p);
-        }
-
-        /**
-         * Initializes the date picker window
-         */
-        private void initDatePicker() {
-            Button dateButton = popupView.findViewById(R.id.datePickerButton);
-            SwitchCompat hasDeadline = popupView.findViewById(R.id.switchDeadline);
-            EditText name = popupView.findViewById(R.id.taskNameForm);
-            calendar = Calendar.getInstance();
-
-            //Display info of the task to popup window
-            name.setText(taskName.getText());
-            if (taskDueDate.getText() == "") {
-                hasDeadline.setChecked(false);
-                dateButton.setTextColor(Color.GRAY);
-                dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
-            }
-            else {
-                hasDeadline.setChecked(true);
-                calendar.setTime(task.getDueDate());
-                dateButton.setTextColor(Color.BLACK);
-                dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.BLACK));
-            }
-            dateButton.setText(format.format(calendar.getTime()));
-
-            //Toggle the button if this task has due date
-            hasDeadline.setOnClickListener(v -> {
-                if (hasDeadline.isChecked()) {
-                    dateButton.setTextColor(Color.BLACK);
-                    dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.BLACK));
-                } else {
-                    dateButton.setTextColor(Color.GRAY);
-                    dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
-                }
-            });
-
-            //Set the calendar date to the date user selected
-            DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
-                hasDeadline.setChecked(true);
-                dateButton.setTextColor(Color.BLACK);
-                dateButton.setBackgroundTintList(ColorStateList.valueOf(Color.BLACK));
-                calendar = Calendar.getInstance();
-                calendar.set(year, month, dayOfMonth);
-                Date date = calendar.getTime();
-                dateButton.setText(format.format(date));
-            };
-
-            //Initialize date picker dialog
-            int year = calendar.get(Calendar.YEAR);
-            int month = calendar.get(Calendar.MONTH);
-            int day = calendar.get(Calendar.DAY_OF_MONTH);
-            datePickerDialog = new DatePickerDialog(itemView.getContext(), dateSetListener, year, month, day);
-            datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-
-            //On clicking on date picker button on popup window, open dialog to choose date.
-            dateButton.setOnClickListener(v -> datePickerDialog.show());
-        }
-
-        /**
-         * Get priority of task
-         */
-        private void initPriority() {
-            RadioGroup radioGroup = popupView.findViewById(R.id.priorityGroup);
-            RadioButton oldChoice = (RadioButton) radioGroup.getChildAt(task.getPriority()-1);
-            oldChoice.setChecked(true);
-
-        }
-
-        /**
-         * Get difficulty of task
-         */
-        private void initDifficulty() {
-            RadioGroup radioGroup = popupView.findViewById(R.id.difficultyGroup);
-            RadioButton oldChoice = (RadioButton) radioGroup.getChildAt(task.getDifficulty()-1);
-            oldChoice.setChecked(true);
         }
     }
 
