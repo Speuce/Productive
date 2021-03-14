2de7b848977aee8f21c97caa4769c81f5763fe8f
Ben Schneider <schnei19@myumanitoba.ca>
2021-03-13 21:55:28 -0600

    Finished tests for StreakRewardManager - fixed bug in StreakRewardManager so a task can't streak with itself

--- app/src/main/java/com/productive6/productive/logic/rewards/impl/StreakRewardManager.java
+++ app/src/main/java/com/productive6/productive/logic/rewards/impl/StreakRewardManager.java
@@ -1,29 +1,36 @@
 package com.productive6.productive.logic.rewards.impl;
 
-import com.productive6.productive.logic.event.EventDispatch;
-import com.productive6.productive.logic.task.ITaskManager;
+import com.productive6.productive.logic.task.ITaskSorter;
 import com.productive6.productive.logic.user.IUserManager;
 import com.productive6.productive.objects.Task;
 import com.productive6.productive.objects.User;
 import com.productive6.productive.objects.events.task.TaskCompleteEvent;
 
+import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 
 
 public class StreakRewardManager extends RewardManager {
 
-    protected ITaskManager taskManager;
+    protected ITaskSorter taskSorter;
+    private int streakValueWeight;
+    private int streakLength;
 
-    public StreakRewardManager(IUserManager data, ITaskManager taskManager, int[] rewardWeights){
+    public StreakRewardManager(IUserManager data, ITaskSorter taskSorter, int[] rewardWeights, int[] streakWeights){
         super(data,rewardWeights[0],rewardWeights[1],rewardWeights[2]);
-        this.taskManager = taskManager;
+        this.streakValueWeight = streakWeights[0];
+        this.streakLength = streakWeights[1];
+        this.taskSorter = taskSorter;
+
     }
 
     protected void taskCompleted(TaskCompleteEvent event){
 
+
         super.taskCompleted(event);
-        taskManager.getCompletedTasks(new StreakConsumer(data,event.getTask()));
+        taskSorter.getCompletedTasks(new StreakConsumer(data,event.getTask(),streakValueWeight,streakLength));
 
     }
 
@@ -31,21 +38,38 @@ public class StreakRewardManager extends RewardManager {
 
         private IUserManager data;
         private Task completedTask;
+        private int streakWeight;
+        private int streakLength;
 
-        public StreakConsumer(IUserManager data, Task completedTask){
+        public StreakConsumer(IUserManager data, Task completedTask, int streakWeight, int streakLength){
             this.data = data;
             this.completedTask = completedTask;
+            this.streakWeight = streakWeight;
+            this.streakLength = streakLength;
         }
 
         @Override
         public void accept(List<Task> tasks) {
-            /**
-             * INSERT TIMING HERE
-             * remove get diff
-             */
-            User person = data.getCurrentUser();
-            person.setCoins(person.getCoins()+1);
-            data.updateUser(person);
+
+            boolean onStreak = false;
+
+            Iterator<Task> taskIterator = tasks.iterator();
+            while(taskIterator.hasNext() && !onStreak){
+                Task curr = taskIterator.next();
+                //calculate period
+                Duration timeDifference = Duration.between(curr.getCompleted(),completedTask.getCompleted());
+                int hours = (int) timeDifference.toHours();
+                if(hours <= streakLength && curr.getId() != completedTask.getId()) //check if with streak window and different tasks
+                    onStreak = true;
+
+            }
+
+            //if streak is confirmed,
+            if(onStreak) {
+                User person = data.getCurrentUser();
+                person.setCoins(person.getCoins() + streakWeight);
+                data.updateUser(person);
+            }
         }
     }
 
--- app/src/test/java/com/productive6/productive/TestStreakRewardManager.java
+++ app/src/test/java/com/productive6/productive/TestStreakRewardManager.java
@@ -4,7 +4,9 @@ import com.productive6.productive.logic.event.EventDispatch;
 import com.productive6.productive.logic.rewards.IRewardManager;
 import com.productive6.productive.logic.rewards.impl.StreakRewardManager;
 import com.productive6.productive.logic.task.ITaskManager;
+import com.productive6.productive.logic.task.ITaskSorter;
 import com.productive6.productive.logic.task.impl.PersistentTaskManager;
+import com.productive6.productive.logic.task.impl.PersistentTaskSorter;
 import com.productive6.productive.logic.user.IUserManager;
 import com.productive6.productive.logic.user.impl.PersistentSingleUserManager;
 import com.productive6.productive.objects.Task;
@@ -15,6 +17,8 @@ import com.productive6.productive.persistence.datamanage.IDataManager;
 import com.productive6.productive.persistence.datamanage.dummy.DummyDataManager;
 import com.productive6.productive.persistence.datamanage.dummy.DummyUserPersistenceManager;
 import com.productive6.productive.persistence.datamanage.impl.InMemoryAndroidDataManager;
+import com.productive6.productive.persistence.datamanage.impl.TaskPersistenceManager;
+import com.productive6.productive.persistence.datamanage.impl.UserPersistenceManager;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,42 +29,52 @@ import static org.junit.Assert.assertEquals;
 public class TestStreakRewardManager {
 
     IUserManager userManager;
-
+    ITaskSorter taskSorter;
     ITaskManager taskManager;
-
     IRewardManager streak;
-
     DummyDataManager dummy;
 
     @Before
     public void init(){
-
+        EventDispatch.clear();
         dummy = new DummyDataManager();
         userManager = new PersistentSingleUserManager(dummy);
         taskManager = new PersistentTaskManager(dummy);
+        taskSorter = new PersistentTaskSorter(dummy);
 
         userManager.load();
 
-        int[] arr = {1,1,100};
-        streak = new StreakRewardManager(userManager,taskManager,arr);
+        int[] rewardArr = {1,1,100};
+        int[] streakArr = {1,24};
+
+        streak = new StreakRewardManager(userManager,taskSorter,rewardArr,streakArr);
         EventDispatch.dispatchEvent(new UserLoadedEvent(userManager.getCurrentUser()));
     }
 
     @Test
-    public void testStreakAdds(){
+    public void testNotOnStreak(){
 
-        EventDispatch.dispatchEvent(new TaskCompleteEvent(new Task("",0,0,0)));
+        Task tempTask = new Task("",1,1,0);
+        taskManager.addTask(tempTask);
+        taskManager.completeTask(tempTask);
+        EventDispatch.dispatchEvent(new TaskCompleteEvent(tempTask));
 
-        assertEquals("Value is not 1", 1, streak.getCoins());
+        assertEquals("Value is not 1",1, streak.getCoins());
 
     }
 
     @Test
-    public void testStreakCombines(){
+    public void testOnStreak(){
 
-        EventDispatch.dispatchEvent(new TaskCompleteEvent(new Task("",1,1,0)));
+        Task tempTask1 = new Task("x",10,10,0);
+        Task tempTask2 = new Task("y",10,10,0);
 
-        assertEquals("Value is not 2", 2, streak.getCoins());
+        taskManager.addTask(tempTask1);
+        taskManager.addTask(tempTask2);
+        taskManager.completeTask(tempTask1);
+        taskManager.completeTask(tempTask2);
+
+        assertEquals("Value is not 1", 21, streak.getCoins());
 
     }
 }
