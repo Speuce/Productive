854dfaecf51e6fbb5cd93cf714af4c5f8609da06
dntrng <pdtrang18899@gmail.com>
2021-03-17 19:06:42 -0500

    Fix UI of edit tasks and popup window

--- app/src/main/java/com/productive6/productive/ui/dashboard/TaskAdapter.java
+++ app/src/main/java/com/productive6/productive/ui/dashboard/TaskAdapter.java
@@ -1,31 +1,17 @@
 package com.productive6.productive.ui.dashboard;
 
-import android.app.DatePickerDialog;
-import android.content.Context;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
-import android.text.InputType;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.SwitchCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.productive6.productive.R;
@@ -36,13 +22,9 @@ import com.productive6.productive.objects.events.ProductiveEventHandler;
 import com.productive6.productive.objects.events.ProductiveListener;
 import com.productive6.productive.objects.events.task.TaskCreateEvent;
 
-import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -156,6 +138,9 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> im
         holder.taskPriority.setText("" + tasks.get(position).getPriorityInString());
         holder.taskDifficulty.setText("" + tasks.get(position).getDifficultyInString());
         if (tasks.get(position).getDueDate() != null) {
+            holder.taskDueDate.setVisibility(View.VISIBLE);
+            holder.dueImageView.setVisibility(View.VISIBLE);
+            holder.dueText.setVisibility(View.VISIBLE);
             holder.taskDueDate.setText(formatter.format(tasks.get(position).getDueDate()));
         } else {
             holder.taskDueDate.setVisibility(View.INVISIBLE);
