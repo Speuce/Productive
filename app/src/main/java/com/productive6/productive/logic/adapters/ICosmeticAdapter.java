40014c3d0bc581706c67c1f6ad2650f0f434e897
Ben Schneider <schnei19@myumanitoba.ca>
2021-03-31 20:55:18 -0500

    added CosmeticAdapter

---
+++ app/src/main/java/com/productive6/productive/logic/adapters/ICosmeticAdapter.java
@@ -0,0 +1,9 @@
+package com.productive6.productive.logic.adapters;
+
+import com.productive6.productive.objects.Cosmetic;
+
+public interface ICosmeticAdapter {
+
+    Cosmetic idToCosmetic(int id);
+
+}
