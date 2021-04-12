398843a6de8dea27435fa672db6c0cd2d1ffa597
Speuce <mrk5199@gmail.com>
2021-04-12 10:05:21 -0500

    Update AllSystem.java

--- app/src/androidTest/java/com/productive6/productive/AllSystem.java
+++ app/src/androidTest/java/com/productive6/productive/AllSystem.java
@@ -10,11 +10,17 @@ import com.productive6.productive.integration.TaskManagerIntTest;
 import com.productive6.productive.integration.TaskSorterIntTest;
 import com.productive6.productive.integration.TitleManagerIntTest;
 import com.productive6.productive.integration.UserManagerIntTest;
+import com.productive6.productive.logic.rewards.impl.RewardManager;
 import com.productive6.productive.system.AddTaskTest;
+import com.productive6.productive.system.BuyCosmeticTest;
 import com.productive6.productive.system.CalanderTest;
+import com.productive6.productive.system.CategorizeTaskTest;
+import com.productive6.productive.system.EditTaskTest;
 import com.productive6.productive.system.NotificationTest;
 import com.productive6.productive.system.ProductivityTest;
+import com.productive6.productive.system.RewardTest;
 import com.productive6.productive.system.SortingTest;
+import com.productive6.productive.system.StreakTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -25,10 +31,15 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         AddTaskTest.class,
-        NotificationTest.class,
+        BuyCosmeticTest.class,
         CalanderTest.class,
+        CategorizeTaskTest.class,
+        EditTaskTest.class,
+        NotificationTest.class,
         ProductivityTest.class,
-        SortingTest.class
+        RewardTest.class,
+        SortingTest.class,
+        StreakTest.class
 })
 @LargeTest
 public class AllSystem {
